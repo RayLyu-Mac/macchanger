682447744db64010d4017192f341f66c4dbe52e7
raylyu <59774755+RayLyu-Mac@users.noreply.github.com>
2023-01-17 20:18:51 -0500

    0117

--- lib/Pages/mainScreen.dart
+++ lib/Pages/mainScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'theme.dart';
+import 'primaryIntro.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -13,7 +14,65 @@ class _LandingPageState extends State<LandingPage> {
   String map_display = "park";
   double _screenWidth = 0;
   double _screenH = 0;
-
+  List posinfo = [
+    [0, 0, 0.5, 0.5],
+    [0, 0.5, 0, 0.5]
+  ];
+  List parkInfo = [
+    [
+      "ast/img/abfall.jpg",
+      "Albion Falls",
+      "At 62 ft., Albion Falls is considered to be one of the most beautiful waterfalls in the Hamilton community, whose classic ‘cascade’ waterway features picturesque observation points, leisurely hiking trails, and family-friendly picnic spots.",
+      Icon(Icons.waterfall_chart),
+      MaterialStateProperty.all(Colors.lightBlueAccent),
+      "https://nature.mcmaster.ca/area/albion-falls/",
+    ],
+    [
+      "ast/img/bfpk.jpg",
+      "Bayfront Park",
+      "Found along the banks of Lake Ontario, Bayfront Park is a 40-acre park in the West-end of Hamilton Harbour. Connected to the beautiful Hamilton Waterfront Trail, specifically the Desjardins Trail, the park is a great place to start or end a waterfront stroll. ",
+      Icon(Icons.padding),
+      MaterialStateProperty.all(Colors.blue),
+      "https://nature.mcmaster.ca/area/bayfront-park/",
+      ["Lake", "Bus", "Bike", "Boating", "Biking"]
+    ],
+    [
+      "ast/img/trail.jpg",
+      "Chedoke Radial Trail",
+      "The Chedoke Radial Trail is a 2.7-kilometre section of the Bruce Trail that traverses the Niagara Escarpment.",
+      Icon(Icons.directions_walk),
+      MaterialStateProperty.all(Colors.greenAccent),
+      "https://nature.mcmaster.ca/area/chedoke-radial-trail/",
+      ["Trail", "Forest", "Bike", "Bus", "Hiking", "Biking"]
+    ],
+    [
+      "ast/img/cfbch.jpg",
+      "Confederation Beach Park",
+      "Confederation Beach Park is a 93-hectare park owned by the City of Hamilton and managed by the Hamilton Conservation Authority.",
+      Icon(Icons.beach_access),
+      MaterialStateProperty.all(Colors.lightBlue),
+      "https://nature.mcmaster.ca/area/confederation-park/",
+      ["Lake", "Car", "Boating", "Biking"]
+    ],
+    [
+      "ast/img/djt.jpg",
+      "Desjardins Trail",
+      "Open all year round, the Desjardins Canal is a great area for runners, bikers, walkers, rollerbladers, skateboarders and hikers alike!",
+      Icon(Icons.light),
+      MaterialStateProperty.all(Colors.orange),
+      "https://nature.mcmaster.ca/area/desjardins-trail/",
+      ["Trail", "Bike", "Car", "Hiking", "Biking"]
+    ],
+    [
+      "ast/img/dpfall.jpg",
+      "Devils Punchbowl",
+      "Consisting of colourful layers of stratified rock, Devil’s Punchbowl is widely considered to be one of the Escarpment’s most unforgettable sights.",
+      Icon(Icons.waterfall_chart_rounded),
+      MaterialStateProperty.all(Colors.lightBlue),
+      "https://nature.mcmaster.ca/area/devils-punchbowl/",
+      ["Fall", "Car", "Hiking"]
+    ]
+  ];
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -48,7 +107,21 @@ class _LandingPageState extends State<LandingPage> {
                               ? "ast/img/greenspace.jpg"
                               : "ast/img/park.jpg"),
                           fit: BoxFit.cover),
-                    )))
+                    ))),
+            for (var i = 0; i < posinfo[0].length; i++)
+              Positioned(
+                  top: posinfo[0][i] * _screenH,
+                  left: posinfo[1][i] * _screenWidth,
+                  child: Container(
+                      height: _screenH / 2,
+                      width: _screenWidth / 2,
+                      child: primIntro(
+                          contents: parkInfo[i][2],
+                          title: parkInfo[i][1],
+                          themeC: parkInfo[i][4],
+                          url: parkInfo[i][5],
+                          img: parkInfo[i][0],
+                          butIcon: parkInfo[i][3])))
           ],
         ),
       ),
--- lib/Pages/primaryIntro.dart
+++ lib/Pages/primaryIntro.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class primIntro extends StatefulWidget {
-  final List contents;
+  final String contents;
+  final String title;
   final String img;
   final String url;
-  final Colors themeC;
+  final MaterialStateProperty<Color> themeC;
+  final Icon butIcon;
   const primIntro(
       {required this.contents,
+      required this.title,
       required this.themeC,
       required this.url,
       required this.img,
+      required this.butIcon,
       super.key});
 
   @override
@@ -37,23 +41,39 @@ class _primIntroState extends State<primIntro> {
                   child: Opacity(
                       opacity: a1.value,
                       child: SimpleDialog(
+                        contentPadding: const EdgeInsets.all(8),
                         shape: Border.all(
                           width: 4,
                           style: BorderStyle.solid,
                           color: Colors.white,
                         ),
-                        title: widget.contents[0],
+                        title: Text(
+                          widget.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "honey",
+                          ),
+                        ),
                         children: [
                           Image(image: AssetImage(widget.img)),
                           const SizedBox(
                             height: 5,
                           ),
-                          Text(widget.contents[1]),
-                          ElevatedButton(
+                          Text(
+                            widget.contents,
+                          ),
+                          ElevatedButton.icon(
+                              icon: widget.butIcon,
                               onPressed: (() {
                                 _launchURL(widget.url);
                               }),
-                              child: const Text("More Info"))
+                              label: const Text(
+                                "More Info",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: "honey"),
+                              ))
                         ],
                       )));
             });
